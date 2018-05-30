6be05b44e57d9563b17fc7860dd8974fcc49644e
yuanyuneee <matcha3@163.com>
2018-05-30 10:33:23 +0800

    数据

---
+++ iQIYI/mock.js
@@ -0,0 +1,34 @@
+const thumbnail = {
+    // data: [{
+    images: [{
+            thumbnail: 'https://wx4.sinaimg.cn/mw1024/006TJMpMly1frsikx2rinj30u01hchdt.jpg',
+            video_id: '0001',
+            video_url: 'http://www.iqiyi.com/a_19rrgzgect.html',
+            title: '泡沫之夏',
+            desc: '世界上最灿烂的烟火，就是曾经以为自己拥有一切，结果却是一场泡沫',
+            update: '更新到第24集',
+            actor: '张雪迎、秦俊杰'
+        }, {
+            thumbnail: 'https://wx4.sinaimg.cn/mw690/006OMfUzly1frm77mrtxzj31p713be5q.jpg',
+            video_id: '0002',
+            video_url: 'http://www.iqiyi.com/a_19rrh9kotx.html',
+            title: '归去来',
+            desc: '从心出发，以心相待[心]',
+            update: '更新到第12集',
+            actor: '唐嫣、罗晋'
+        }, {
+            thumbnail: 'http://wx4.sinaimg.cn/bmiddle/69e883c3ly1frs1fyfw4vj20u019ynpe.jpg',
+            video_id: '0003',
+            video_url: 'http://www.iqiyi.com/a_19rrh1hvkt.html',
+            title: '家有儿女初长成',
+            desc: '留一份对静谧的虔诚深爱你的承蒙念一席暖情怀的温风',
+            update: '更新到第2集',
+            actor: '张一山、高亚麟'
+        }]
+        // }]
+}
+
+
+export {
+    thumbnail
+}
