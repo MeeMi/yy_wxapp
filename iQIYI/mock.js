c9f93e85b3fd2d217e77320f1107c828107a5edf
yuanyuneee <matcha3@163.com>
2018-06-05 00:56:39 +0800

    修改数组

--- iQIYI/mock.js
+++ iQIYI/mock.js
@@ -24,11 +24,148 @@ const thumbnail = {
             desc: '留一份对静谧的虔诚深爱你的承蒙念一席暖情怀的温风',
             update: '更新到第2集',
             actor: '张一山、高亚麟'
+        }, {
+            thumbnail: 'http://wx4.sinaimg.cn/bmiddle/69e883c3ly1frs1fyfw4vj20u019ynpe.jpg',
+            video_id: '0004',
+            video_url: 'http://www.iqiyi.com/a_19rrh1hvkt.html',
+            title: '家有儿女初长成22222',
+            desc: '留一份对静谧的虔诚深爱你的承蒙念一席暖情怀的温风',
+            update: '更新到第2集',
+            actor: '张一山、高亚麟'
+        }, {
+            thumbnail: 'http://wx4.sinaimg.cn/bmiddle/69e883c3ly1frs1fyfw4vj20u019ynpe.jpg',
+            video_id: '0005',
+            video_url: 'http://www.iqiyi.com/a_19rrh1hvkt.html',
+            title: '家有儿女初长成33333',
+            desc: '留一份对静谧的虔诚深爱你的承蒙念一席暖情怀的温风',
+            update: '更新到第2集',
+            actor: '张一山、高亚麟'
+        }, {
+            thumbnail: 'http://wx4.sinaimg.cn/bmiddle/69e883c3ly1frs1fyfw4vj20u019ynpe.jpg',
+            video_id: '0006',
+            video_url: 'http://www.iqiyi.com/a_19rrh1hvkt.html',
+            title: '家有儿女初长成44444',
+            desc: '留一份对静谧的虔诚深爱你的承蒙念一席暖情怀的温风',
+            update: '更新到第2集',
+            actor: '张一山、高亚麟'
+        }, {
+            thumbnail: 'http://wx4.sinaimg.cn/bmiddle/69e883c3ly1frs1fyfw4vj20u019ynpe.jpg',
+            video_id: '0007',
+            video_url: 'http://www.iqiyi.com/a_19rrh1hvkt.html',
+            title: '家有儿女初长成55555',
+            desc: '留一份对静谧的虔诚深爱你的承蒙念一席暖情怀的温风',
+            update: '更新到第2集',
+            actor: '张一山、高亚麟'
         }]
         // }]
 }
 
+const vedios = {
+    vedio: {
+        hot: [{
+            title: '热门1',
+            hot_id: '0001',
+            hot_url: 'http://vjs.zencdn.net/v/oceans.mp4 ',
+            watch_num: '83万',
+            like_num: 2267
+        }, {
+            title: '热门2',
+            hot_id: '0003',
+            hot_url: 'http://vjs.zencdn.net/v/oceans.mp4 ',
+            watch_num: '11.5万',
+            like_num: 2267
+        }, {
+            title: '热门3',
+            hot_id: '0003',
+            hot_url: 'http://ohjdda8lm.bkt.clouddn.com/course/sample1.mp4',
+            watch_num: '1.9万',
+            like_num: 2267
+        }, {
+            title: '热门4',
+            hot_id: '0001',
+            hot_url: 'http://vjs.zencdn.net/v/oceans.mp4 ',
+            watch_num: '83万',
+            like_num: 2267
+        }, {
+            title: '热门5',
+            hot_id: '0003',
+            hot_url: 'http://vjs.zencdn.net/v/oceans.mp4 ',
+            watch_num: '11.5万',
+            like_num: 2267
+        }, {
+            title: '热门6',
+            hot_id: '0003',
+            hot_url: 'http://ohjdda8lm.bkt.clouddn.com/course/sample1.mp4',
+            watch_num: '1.9万',
+            like_num: 2267
+        }, {
+            title: '热门7',
+            hot_id: '0003',
+            hot_url: 'http://ohjdda8lm.bkt.clouddn.com/course/sample1.mp4',
+            watch_num: '1.9万',
+            like_num: 2267
+        }],
+        amuse: [{
+            title: '搞笑1',
+            hot_id: '0001',
+            hot_url: 'http://vjs.zencdn.net/v/oceans.mp4 ',
+            watch_num: '83万',
+            like_num: 2267
+        }, {
+            title: '搞笑2',
+            hot_id: '0003',
+            hot_url: 'http://vjs.zencdn.net/v/oceans.mp4 ',
+            watch_num: '11.5万',
+            like_num: 2267
+        }, {
+            title: '搞笑3',
+            hot_id: '0003',
+            hot_url: 'http://ohjdda8lm.bkt.clouddn.com/course/sample1.mp4',
+            watch_num: '1.9万',
+            like_num: 2267
+        }],
+        variety: [{
+            title: '综艺1',
+            hot_id: '0001',
+            hot_url: 'http://vjs.zencdn.net/v/oceans.mp4 ',
+            watch_num: '83万',
+            like_num: 2267
+        }, {
+            title: '综艺2',
+            hot_id: '0003',
+            hot_url: 'http://vjs.zencdn.net/v/oceans.mp4 ',
+            watch_num: '11.5万',
+            like_num: 2267
+        }, {
+            title: '综艺3',
+            hot_id: '0003',
+            hot_url: 'http://ohjdda8lm.bkt.clouddn.com/course/sample1.mp4',
+            watch_num: '1.9万',
+            like_num: 2267
+        }],
+        information: [{
+            title: '资讯1',
+            hot_id: '0001',
+            hot_url: 'http://vjs.zencdn.net/v/oceans.mp4 ',
+            watch_num: '83万',
+            like_num: 2267
+        }, {
+            title: '资讯2',
+            hot_id: '0003',
+            hot_url: 'http://vjs.zencdn.net/v/oceans.mp4 ',
+            watch_num: '11.5万',
+            like_num: 2267
+        }, {
+            title: '资讯3',
+            hot_id: '0003',
+            hot_url: 'http://ohjdda8lm.bkt.clouddn.com/course/sample1.mp4',
+            watch_num: '1.9万',
+            like_num: 2267
+        }]
+    }
 
+}
 export {
-    thumbnail
+    thumbnail,
+    vedios
 }
