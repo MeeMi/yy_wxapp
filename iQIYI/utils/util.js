55ba437e1e23fc3640c19a846bf9681067cf6554
yuanyuneee <matcha3@163.com>
2018-06-05 01:00:28 +0800

    添加加载刷新方法

--- iQIYI/utils/util.js
+++ iQIYI/utils/util.js
@@ -14,19 +14,59 @@ const DEFAULT_REQUEST_OPTIONS = {
 let util = {
     request(opt) {
         let options = Object.assign({}, DEFAULT_REQUEST_OPTIONS, opt);
-        console.log(options);
+        // console.log(options.data.page);
         let { url, data, header, method, dataType, mock = false } = options;
-        console.log(url, data, header, method, dataType, mock)
+        // console.log(url, data, header, method, dataType, mock)
 
         return new Promise((resolve, reject) => {
+
             if (mock) {
+
+                if (data.hasOwnProperty('page') && data.hasOwnProperty('datasize')) {
+                    let res = null;
+                    let page = data.page;
+                    console.log(page)
+                    let datasize = data.datasize;
+                    const types = ['hot', 'amuse', 'variety', 'information'];
+                    let type = types[data.type - 1];
+                    let returnData = [];
+
+                    const allRes = Mock[url][data.tag][type];
+                    console.log(allRes)
+                        // console.log(allRes.length)
+
+                    if (page < allRes.length) {
+                        console.log(datasize)
+
+                        for (let i = page; i < page + datasize; i++) {
+                            // console.log(i)
+                            if (allRes[i]) {
+                                returnData.push(allRes[i])
+                            }
+                        }
+                        res = { res: 1, data: returnData };
+                    } else {
+                        res = { res: 0, data: returnData }
+                    }
+
+                    // let res = {
+                    //     statusCode: 200,
+                    //     data: Mock[url]
+                    // }
+                    console.log(res)
+                    resolve(res);
+                    return;
+                }
+
                 let res = {
                     statusCode: 200,
                     data: Mock[url]
                 }
                 resolve(res.data);
                 return;
+
             }
+
 
             wx.request({
                 url,
@@ -44,6 +84,37 @@ let util = {
         })
 
     }
+    // requestDetail(opt) {
+    //     let options = Object.assign({}, DEFAULT_REQUEST_OPTIONS, opt);
+    //     console.log(options);
+
+    //     return new Promise((resolve, reject) => {
+    //         if (mock) {
+    //             let res = {
+    //                 statusCode: 200,
+    //                 data: Mock[url]
+    //             }
+
+    //             resolve(res.data);
+    //             return;
+    //         }
+    //         // 下面是网络请求
+    //         wx.request({
+    //             url,
+    //             data,
+    //             header,
+    //             method,
+    //             dataType,
+    //             success(res) {
+    //                 resolve(res.data)
+    //             },
+    //             fail(err) {
+    //                 reject(err)
+    //             }
+    //         })
+    //     })
+
+    // }
 }
 
 
