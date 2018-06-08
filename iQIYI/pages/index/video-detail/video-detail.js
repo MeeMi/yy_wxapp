29878c31374bae9f58cd36122a75d5abcdd98f56
yuanyuneee <matcha3@163.com>
2018-06-08 23:37:55 +0800

    视频切换

--- iQIYI/pages/index/video-detail/video-detail.js
+++ iQIYI/pages/index/video-detail/video-detail.js
@@ -8,30 +8,42 @@ Page({
     data: {
         video_id: '',
         isListHidden: true,
-        isDescHidden: false
+        isDescHidden: true,
+        isLoading: false,
+        isSelected: 1,
+        mediaList: null,
+        playerUrl: ''
     },
 
     onLoad: function(option) {
         console.log('页面 传过来的是' + option.id);
 
         this.setData({
-                video_id: option.id
-            })
-            // setTimeout(this.requestVideo(), 1000)
-    },
-    // requestVideo: function() {
-    //     util.requestDetail({
-    //             url: `thumbnail`,
-    //             mock: true,
-    //             data: {
-    //                 id: this.data.video_id,
-    //                 langs: 'en'
-    //             }
-    //         })
-    //         .then(res => {
+            video_id: option.id,
+            isLoading: true,
+            mediaList: null
+        })
 
-    //         })
-    // }
+        this.requestVideo();
+    },
+    requestVideo: function() {
+        util.request({
+                url: `https://www.easy-mock.com/mock/5b0c37bed0e51c310ce24ab0/iqiyi/media#!method=get`,
+                mock: false,
+                data: {
+                    id: this.data.video_id,
+                    tag: 'dramas',
+                    langs: 'en'
+                }
+            })
+            .then(res => {
+                this.setData({
+                    mediaList: res,
+                    playerUrl: res.drama_num[0].video_url
+                })
+                console.log(this.data.mediaList);
+            })
+    },
     openList: function() {
         console.log('dianji')
         this.setData({
@@ -47,6 +59,26 @@ Page({
         let isDescHidden = !this.data.isDescHidden;
         this.setData({
             isDescHidden
+        })
+    },
+    changenum: function(e) {
+        let mediaList = this.data.mediaList;
+        let playerUrl = this.data.playerUrl
+            // for (let i = 1; i < dramas.length; i++) {
+            //     dramas[i - 1].selected == parseInt(i) === e.currentTarget.dataset.num;
+            // console.log(dramas)            
+            // }
+
+        for (let i of mediaList.drama_num) {
+            i.selected = parseInt(i.drama_id) === e.currentTarget.dataset.num + 1
+            if (i.selected) {
+                playerUrl = i.video_url
+            }
+        }
+        console.log(mediaList)
+        this.setData({
+            mediaList,
+            playerUrl
         })
     }
 })
