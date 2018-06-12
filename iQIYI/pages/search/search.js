e70de4c666ce239a48a3d99aff7fa5b5050fbc22
yuanyuneee <matcha3@163.com>
2018-06-12 23:54:22 +0800

    完善搜索功能

--- iQIYI/pages/search/search.js
+++ iQIYI/pages/search/search.js
@@ -7,19 +7,30 @@ Page({
      */
     data: {
         searchVal: "",
-        result: [],
-        hiddenList: true
+        result: []
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-
+        this.setData({
+            searchVal: options.key
+        })
+        console.log(getCurrentPages())
+    },
+    //这里是页面B的 onShow 方法
+    onShow: function() {
+        var that = this
+            //如果 isBack 为 true，就返回上一页
+        if (that.data.isBack) {
+            wx.navigateBack()
+        }
     },
     bindKeyInput: function(e) {
+        console.log(e.detail.value)
         this.setData({
-            searchVal: e.detail.value
+            searchVal: e.detail.value.trim()
         })
         this.search();
     },
@@ -31,24 +42,36 @@ Page({
         })
     },
     cancel: function() {
+        console.log('fanhui')
         wx.navigateBack({
             delta: 1
         })
+        this.setData({
+            searchVal: ''
+        })
+
     },
     search: function() {
-        util.request({
-                url: `https://www.easy-mock.com/mock/5b0c37bed0e51c310ce24ab0/iqiyi/media#!method=get`,
-                mock: false,
-                data: {
-                    key: this.data.searchVal
-                }
-            })
-            .then(res => {
-                this.setData({
-                    result: res
+        if (this.data.searchVal != '') {
+            util.request({
+                    url: `https://www.easy-mock.com/mock/5b0c37bed0e51c310ce24ab0/iqiyi/media#!method=get`,
+                    mock: false,
+                    data: {
+                        key: this.data.searchVal
+                    }
                 })
-                console.log(this.data.result)
+                .then(res => {
+                    this.setData({
+                        result: res
+                    })
+                    console.log(this.data.result)
+                })
+        } else {
+            this.setData({
+                result: []
             })
+        }
+
     },
     resultPage: function() {
         this.search();
@@ -58,30 +81,42 @@ Page({
     },
     clickResult: function(e) {
         let index = e.currentTarget.dataset.num;
+        let searchVal = this.data.searchVal;
         let StorageResult = [];
+        let temp = [];
         const result = this.data.result;
+
         for (let i = 0; i < result.length; i++) {
             if (i == index) {
-                StorageResult = result.splice(i, 1);
+                temp = result.splice(i, 1);
             }
         }
-        StorageResult.push(result)
-        wx.getStorage({
-            key: 'String',
+        for (let i in result) {
+            StorageResult = [...temp, result[i]]
+        }
+        wx.setStorage({
+            key: 'searchResult',
+            data: StorageResult,
             success: function(res) {
-                // success
-            },
-            fail: function() {
-                // fail
-            },
-            complete: function() {
-                // complete
+                wx.navigateTo({
+                    url: `search-result/search-result?key=${searchVal}`
+                })
             }
         })
-        wx.navigateTo({
-            url: `search-result/search-result`
-        })
+    },
+    resultPage: function() {
+        let searchVal = this.data.searchVal;
 
-    }
+        wx.setStorage({
+            key: 'searchResult',
+            data: this.data.result,
+            success: function(res) {
+                wx.navigateTo({
+                    url: `search-result/search-result?key=${searchVal}`
+                })
+            }
+        })
+    },
+
 
 })
