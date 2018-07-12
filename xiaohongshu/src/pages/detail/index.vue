6485f4f0b058bb9cc4aa7c274ca85e22769f6c99
yuanyuneee <matcha3@163.com>
2018-07-12 23:14:05 +0800

    内容详情页面

--- xiaohongshu/src/pages/detail/index.vue
+++ xiaohongshu/src/pages/detail/index.vue
@@ -1,23 +1,41 @@
 <template>
     <div>
-       <note-detail :detailImgs="detailImgs" ></note-detail>
+        <!-- 传给NoteDetail组件  -->
+       <note-detail :swiperImg="swiperImg" :authorInfo="authorInfo" :contentInfo="contentInfo" :commentItem="commentItem"></note-detail>
     </div>
 </template>
 
 <script>
 import NoteDetail from '@/components/NoteDetail'
+import fly from '@/utils/fly'
 export default {
      components: {
-          NoteDetail
+          NoteDetail,
      },
     data(){
         return {
-            detailImgs:[{id:1,src:'https://ci.xiaohongshu.com/8ea674e4-12e9-4240-841a-82705a0a5263@r_750w_750h_ss1.jpg'},{id:2,src:'https://ci.xiaohongshu.com/5e9f53e4-5773-413c-9eb8-0aeadfafb48c@r_750w_750h_ss1.jpg'},
-           {id:3,src:'https://ci.xiaohongshu.com/805b86e1-2103-4376-887b-eb2413ffd579@r_750w_750h_ss1.jpg'}]
+            swiperImg:[],
+            authorInfo:{},
+            contentInfo:{},
+            commentItem:[]
         }
     },
-    methods:{
-       
+    onLoad (options) {
+        fly.get('noteDetail#!method=get').then((res)=>{
+            for(let i of res.data){
+                // 笔记id 跟参数对比 然后给noteDtail
+                if(i.id == options.id){
+                    this.swiperImg = i.swiperImg;
+                    this.authorInfo = i.authorInfo;
+                    this.contentInfo = i.contentInfo;
+                    this.commentItem = i.commentItem;
+                }
+            }
+            console.log(this.commentItem)
+          })
+          .catch((e)=>{
+            console.log(e)
+          })
     }
 }
 </script>
